5b5a0492f660a2975fdfad1a1ea7652ef93986b3
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-10-02 20:14:20 +0500

    custom clipper

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+
 class LoginUi extends StatefulWidget {
   const LoginUi({super.key});
 
@@ -9,6 +11,67 @@ class LoginUi extends StatefulWidget {
 class _LoginUiState extends State<LoginUi> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                Color(0xFF0f3557),
+                Color(0xFF07c78b8),
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            children: <Widget>[
+              ClipPath(
+                clipper: Backgroundclipper(),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    // borderRadius: BorderRadius.only(
+                    //   bottomRight: Radius.circular(150),
+                    // ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Backgroundclipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    // TODO: implement getClip
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width - 20, size.height - 250);
+    // path.quadraticBezierTo(
+    //     size.width-50, size.height, size.width , size.height - 150);
+    var controlpoint = new Offset(size.width, 0);
+    var endpoint = new Offset(size.width - 20, size.height - 250);
+    path.quadraticBezierTo(
+        controlpoint.dx, controlpoint.dy, endpoint.dx, endpoint.dy);
+    path.lineTo(size.width / 2, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
   }
 }

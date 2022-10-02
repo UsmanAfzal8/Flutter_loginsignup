615d301d51e263301514d322a5975101ef2cd430
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-10-02 16:28:24 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'screen/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginUi(),
+    );
+  }
+}
---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class LoginUi extends StatefulWidget {
+  const LoginUi({super.key});
+
+  @override
+  State<LoginUi> createState() => _LoginUiState();
+}
+
+class _LoginUiState extends State<LoginUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
